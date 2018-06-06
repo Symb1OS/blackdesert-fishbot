5d2cb60a038fcf94065daed1b334e306fffe326a
symbios <dxeqtr@gmail.com>
2018-06-06 19:06:23 +0300

    readme

--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 
 - [JDK 8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
 - [Arduino IDE](https://www.arduino.cc/en/main/software)
-- [Python 3.6](https://www.python.org/downloads/release/python-360/)
+- [Python 3.5](https://www.python.org/downloads/release/python-360/)
 - [Pytorch](https://pytorch.org/)
 
 ### Presets
--- script/install.bat
+++ script/install.bat
@@ -1,12 +1,12 @@
 @echo off 
 
-set /p pyi= Install Python? y/n
+set /p pyi= Install Python? (y/n) 
 if %pyi%==y (echo Installing Python... & soft\python-3.5.4.exe)
 
-set /p ai= Install Arduino IDE? y/n
+set /p ai= Install Arduino IDE? (y/n) 
 if %ai%==y (echo Installing Arduino IDE.. & soft\arduino-1.8.5-windows.exe)
 
-set /p pi= Install pip dependency? y/n
+set /p pi= Install pip dependency? (y/n) 
 if %pi%==y (echo Install dependency & pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp35-cp35m-win_amd64.whl & pip install torchvision)
 
 pause
