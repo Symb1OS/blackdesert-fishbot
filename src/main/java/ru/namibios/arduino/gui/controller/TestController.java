f04a86f0228ea4aaf32b08c82f94c90445cfa28b
symbios <dxeqtr@gmail.com>
2018-11-09 18:14:19 +0300

    alternative emulation methods

--- src/main/java/ru/namibios/arduino/gui/controller/TestController.java
+++ src/main/java/ru/namibios/arduino/gui/controller/TestController.java
@@ -1,9 +1,11 @@
 package ru.namibios.arduino.gui.controller;
 
+import org.apache.log4j.Logger;
+import ru.namibios.arduino.utils.WinFunction;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import org.apache.log4j.Logger;
+import java.awt.event.KeyEvent;
 
 public class TestController implements ActionListener{
 
@@ -16,6 +18,22 @@ public class TestController implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		logger.info("Action for button..");
+
+		WinFunction.sendToInput(KeyEvent.VK_SPACE);
+
+		WinFunction.sendToInput("wasdr");
+
+		WinFunction.clickLeft(500, 500);
+
+		WinFunction.robotSend(KeyEvent.VK_SPACE);
+		WinFunction.robotSend(KeyEvent.VK_W);
+		WinFunction.robotSend(KeyEvent.VK_A);
+		WinFunction.robotSend(KeyEvent.VK_S);
+		WinFunction.robotSend(KeyEvent.VK_D);
+		WinFunction.robotSend(KeyEvent.VK_R);
+
+		WinFunction.robotClick(500, 500, KeyEvent.BUTTON1_MASK);
+
 	}
 
 }
