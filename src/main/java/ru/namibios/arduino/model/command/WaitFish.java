db6d34ca2f0dd8fa4b26f1846143aa84c292e065
symbios <dxeqtr@gmail.com>
2018-11-15 12:10:51 +0300

    For correctly work cut

--- src/main/java/ru/namibios/arduino/model/command/WaitFish.java
+++ src/main/java/ru/namibios/arduino/model/command/WaitFish.java
@@ -33,7 +33,7 @@ public class WaitFish implements Command{
 
 		screen = new Screen(region);
 		imageParser = new ImageParser(screen, Chars.values());
-		imageParser.parse(Screen.WHITE);
+		imageParser.parse(Screen.GRAY);
 
 //		MatrixUtils.printMatrix(imageParser.getImageMatrix(), "0");
 
