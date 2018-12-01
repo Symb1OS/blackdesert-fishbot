5d2b3accca501e37a9420dba77378f538ed438ed
symbios <dxeqtr@gmail.com>
2018-12-01 20:27:30 +0300

    Random delay. Trash collection.

--- src/main/java/ru/namibios/arduino/model/state/service/input/emulation/AWTRobot.java
+++ src/main/java/ru/namibios/arduino/model/state/service/input/emulation/AWTRobot.java
@@ -4,10 +4,13 @@ import ru.namibios.arduino.config.Application;
 import ru.namibios.arduino.utils.DelayUtils;
 
 import java.awt.*;
+import java.util.Random;
 
 public class AWTRobot extends AbstractEmulationInput {
 
     private Robot robot;
+
+    private Random random = new Random();
 
     public AWTRobot() {
         try {
@@ -33,7 +36,7 @@ public class AWTRobot extends AbstractEmulationInput {
     @Override
     public void pressKey(int key) {
         robot.keyPress(key);
-        DelayUtils.delay(Application.getInstance().PRESS_KEY_DELAY());
+        DelayUtils.delay(random.nextInt(50) + Application.getInstance().PRESS_KEY_DELAY());
         robot.keyRelease(key);
 
     }
