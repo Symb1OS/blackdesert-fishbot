0f8b6d8612a38c3bb97cec352b994f45054cda7a
symbios <dxeqtr@gmail.com>
2019-04-12 15:13:15 +0300

    Checks

--- src/main/java/ru/namibios/arduino/gui/Launcher.java
+++ src/main/java/ru/namibios/arduino/gui/Launcher.java
@@ -28,80 +28,129 @@ public class Launcher {
 		LOCALES.put("Русский", new Locale("ru", "RU"));
 	}
 
-	private static boolean hostAvailabilityCheck() {
-
-		try(Socket s = new Socket()) {
-
-			String url = Application.getInstance().URL_CAPTCHA_SERVICE();
-			String ip = url.substring(0, url.indexOf(":"));
-			String port = url.substring(url.indexOf(":") + 1 );
-
-			s.connect(new InetSocketAddress(ip, Integer.valueOf(port)), 5000);
-			return true;
-		} catch (IOException e) {
-			LOG.error(ExceptionUtils.getString(e));
-		}
-		return false;
-	}
-
-	private static void checkIfRunning() {
-
-		try {
-
-			socket = new ServerSocket(PORT,0, InetAddress.getByAddress(new byte[] {127,0,0,1}));
-		}
-
-		catch (BindException e) {
-            JOptionPane.showMessageDialog(null, "Program already running on port: " + Application.getInstance().LOCAL_PORT(), "Warning", JOptionPane.ERROR_MESSAGE);
-			LOG.error("Program already running on port: " + Application.getInstance().LOCAL_PORT());
-			LOG.error(ExceptionUtils.getString(e));
-			Application.closeBot(Application.CODE_ALREADY_RUNNING);
-		}
-		catch (IOException e) {
-			LOG.error("Unexpected error.");
-			LOG.error(ExceptionUtils.getString(e));
-			Application.closeBot(Application.CODE_ALREADY_RUNNING);
-		}
-	}
-
-	private static void checkResolution() {
-		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-		int width = gd.getDisplayMode().getWidth();
-		int height = gd.getDisplayMode().getHeight();
-
-		LOG.debug("width = " + width);
-		LOG.debug("height = " + height);
-
-		if (width != 1920 || height != 1080) {
-			LOG.error("Unsupported resolution: " + width + "x" + height);
-			JOptionPane.showMessageDialog(null, "Supporting only 1920x1080", "Unsupported resolution", JOptionPane.ERROR_MESSAGE);
-			Application.closeBot(Application.CODE_UNSUPPORTED_RESOLUTION);
-		}
-	}
-
 	public static void close(){
 
 		try {
 
 			socket.close();
 
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			LOG.error(ExceptionUtils.getString(e));
 		}
 	}
 
-	public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+	private static abstract class Check {
 
-		boolean isAvailable = hostAvailabilityCheck();
-		if (!isAvailable) {
-			LOG.error(Message.SERVER_NOT_AVAILABLE_EN);
-			JOptionPane.showMessageDialog(null, Message.SERVER_NOT_AVAILABLE_EN, "Warning", JOptionPane.ERROR_MESSAGE);
+		private Check nextCheck;
+
+		public Check setNext(Check nextCheck) {
+			this.nextCheck = nextCheck;
+			return nextCheck;
 		}
 
-		checkIfRunning();
-		checkResolution();
+		protected boolean next(){
+
+			if (nextCheck == null) {
+				return true;
+			}
+
+			return nextCheck.check();
+		}
+
+		public abstract boolean check();
+
+	}
+
+	private static class InstanceCheck extends Check {
+
+		@Override
+		public boolean check() {
+
+			try {
+
+				socket = new ServerSocket(PORT,0, InetAddress.getByAddress(new byte[] {127,0,0,1}));
+			}
+
+			catch (BindException e) {
+				JOptionPane.showMessageDialog(null, "Program already running on port: " + Application.getInstance().LOCAL_PORT(), "Warning", JOptionPane.ERROR_MESSAGE);
+				LOG.error("Program already running on port: " + Application.getInstance().LOCAL_PORT());
+				LOG.error(ExceptionUtils.getString(e));
+				Application.closeBot(Application.CODE_ALREADY_RUNNING);
+				return false;
+			}
+			catch (IOException e) {
+				LOG.error("Unexpected error.");
+				LOG.error(ExceptionUtils.getString(e));
+				Application.closeBot(Application.CODE_ALREADY_RUNNING);
+				return false;
+			}
+
+			return next();
+		}
+
+	}
+
+	private static class HostAvailabilityCheck extends Check {
+
+		private static final Logger LOG = Logger.getLogger(HostAvailabilityCheck.class);
+
+		@Override
+		public boolean check() {
+
+			try (Socket s = new Socket()) {
+
+				String url = Application.getInstance().URL_CAPTCHA_SERVICE();
+				String ip = url.substring(0, url.indexOf(":"));
+				String port = url.substring(url.indexOf(":") + 1 );
+
+				s.connect(new InetSocketAddress(ip, Integer.valueOf(port)), 5000);
+				return next();
+
+			} catch (IOException e) {
+				LOG.error(ExceptionUtils.getString(e));
+			}
+
+			LOG.error(Message.SERVER_NOT_AVAILABLE_EN);
+			JOptionPane.showMessageDialog(null, Message.SERVER_NOT_AVAILABLE_EN, "Warning", JOptionPane.ERROR_MESSAGE);
+
+			return false;
+		}
+	}
+
+	private static class ResolutionCheck extends Check {
+
+		private static final Logger LOG = Logger.getLogger(ResolutionCheck.class);
+
+		@Override
+		public boolean check() {
+
+			GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+			int width = gd.getDisplayMode().getWidth();
+			int height = gd.getDisplayMode().getHeight();
+
+			LOG.debug("width = " + width);
+			LOG.debug("height = " + height);
+
+			if (width != 1920 || height != 1080) {
+				LOG.error("Unsupported resolution: " + width + "x" + height);
+				JOptionPane.showMessageDialog(null, "Supporting only 1920x1080", "Unsupported resolution", JOptionPane.ERROR_MESSAGE);
+				Application.closeBot(Application.CODE_UNSUPPORTED_RESOLUTION);
+				return false;
+			}
+
+			return next();
+		}
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, IOException {
 
 		LOG.info("Start program..");
+
+		Check checks = new InstanceCheck();
+		checks.setNext(new HostAvailabilityCheck())
+				.setNext(new ResolutionCheck());
+
+		checks.check();
 
         String language = Application.getInstance().LANGUAGE();
         LOG.debug("Language: " + language);
